89c028d7b3ae6d246d833a0fc215c74f71a393de
trampboy495 <histroylover@gmail.com>
2020-07-10 16:34:03 +0530

    [siddhesh] add.UC3 simulates doublet combinations and store their percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -4,7 +4,7 @@ Tails=0
 noOfheads=0
 noOftails=0
 noOfLoops=0
-declare -A coinSinglet
+declare -A coinSinglet coinDoublet
 function Singlet() {
 	while [ $noOfLoops -lt 10 ]
 	do
@@ -22,4 +22,33 @@ function Singlet() {
 	coinSinglet["H"]=$Headpercentage
 	coinSinglet["T"]=$Tailpercentage
 }
+function Doublet() {
+	noOfLoops=0 HH=0 HT=0 TH=0 TT=0
+	while [ $noOfLoops -lt 10 ]
+	do
+		r=`printf "%s\n" {0..1}{0..1} | shuf -n1`
+		case $r in
+			$Heads$Heads)
+				((HH++))
+			;;
+			$Heads$Tails)
+				((HT++))
+			;;
+			$Tails$Heads)
+				((TH++))
+			;;
+			$Tails$Tails)
+				((TT++))
+			;;
+		esac
+		((noOfLoops++))
+	done
+	HHpercentage=$(($HH*10))
+	HTpercentage=$(($HT*10))
+	THpercentage=$(($TH*10))
+	TTpercentage=$(($TT*10))
+	coinDoublet=([HH]=$HHpercentage [HT]=$HTpercentage [TH]=$THpercentage [TT]=$TTpercentage)
+}
 Singlet
+Doublet
+
